78f74dfe026439d0f3682e44e525c5e15a82c83d
igna0797 <igna0797@hotmail.com>
2024-04-28 23:22:55 -0300

    Path changes

--- implementations/sgan/Generador.py
+++ implementations/sgan/Generador.py
@@ -1,20 +1,23 @@
 import torch
 import os
+import argparse
+
 from torchvision.utils import save_image
 from sgan import Generator  # Import your Generator class from Sgan.py
 
-def generate_image_from_seed(seed, save_path=None):
+def generate_image_from_seed(seed, save_path=None,generator_weights_path="../../trainings/n-lineas_0_Random_False/generator_weights.pth"):
     # Set a fixed seed for the random number generator (for reproducibility)
     torch.manual_seed(seed)  # You can use any seed value you like
 
     # Initialize the generator model
     generator = Generator()
 
+    cuda = True if torch.cuda.is_available() else False
     # Load the pre-trained generator weights
-    generator_weights_path = "/content/drive/MyDrive/Redes neuronales/Monografia/generator_weights.pth"
-
-    generator.load_state_dict(torch.load(generator_weights_path))
-
+    if cuda == True :
+        generator.load_state_dict(torch.load(generator_weights_path))
+    else :
+        map_location=torch.device('cpu') 
     # Set the generator in evaluation mode
     generator.eval()
 
@@ -35,11 +38,17 @@ def generate_image_from_seed(seed, save_path=None):
 
 # Example usage:
 # Example usage with different seeds:
-
-seed_value = 1  # Replace with your desired seed value
-output_dir = "/content/drive/MyDrive/Redes neuronales/Monografia/images/"  # Replace with your desired output directory
-output_filename = f"seed_{seed_value:.3f}.png"  # Access the first element of the list
-output_path = os.path.join(output_dir, output_filename)
-
-# Generate images with different seeds
-generated_image = generate_image_from_seed(seed_value,output_path)
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-o ", "--output_path", type=str, default="images/", help="directory for the Image returned by the generator")
+    parser.add_argument("-s","--seed" ,type=int , default=3 ,help="seed for the generator for reproducible results")
+    arguments = parser.parse_args()
+    #old directory : "/content/drive/MyDrive/Redes neuronales/Monografia/images/"
+    seed_value = arguments.seed
+    output_dir = arguments.output_path
+    output_filename = f"seed_{seed_value:.3f}.png"  # Access the first element of the list
+    output_path = os.path.join(output_dir, output_filename)
+    print(output_path)
+    # Generate images with different seeds
+    generator_weights_path = "../../trainings/n-lineas_3_Random_False/generator_weights.pth"
+    generated_image = generate_image_from_seed(seed_value,output_path,generator_weights_path)
