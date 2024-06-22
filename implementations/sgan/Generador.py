97e301b3819fb8daa0c522c1d8fac6e14d706d84
igna <igna0797@gmail.com>
2024-06-22 17:30:44 -0300

    changed path for images

--- implementations/sgan/Generador.py
+++ implementations/sgan/Generador.py
@@ -40,17 +40,18 @@ def generate_image_from_seed(seed, save_path=None,generator_weights_path="../../
 # Example usage with different seeds:
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("-o ", "--output_path", type=str, default="images/", help="directory for the Image returned by the generator")
+    parser.add_argument("-o ", "--output_path", type=str, default="../../images/", help="directory for the Image returned by the generator")
     parser.add_argument("-s","--seed" ,type=int , default=3 ,help="seed for the generator for reproducible results")
     parser.add_argument("-w ", "--weights_path", type=str, default="../../trainings/n-lineas_3_Random_False/generator_weights.pth", help="directory for the weigths of the generator")
     arguments = parser.parse_args()
     #old directory : "/content/drive/MyDrive/Redes neuronales/Monografia/images/"
+    abs_dir = os.path.dirname(os.path.abspath(__file__))
+    
     seed_value = arguments.seed
-    output_dir = arguments.output_path
+    output_dir = os.path.join(abs_dir, arguments.output_path)
     output_filename = f"seed_{seed_value:.3f}.png"  # Access the first element of the list
     output_path = os.path.join(output_dir, output_filename)
     print(output_path)
     # Generate images with different seeds
-    abs_dir = os.path.dirname(os.path.abspath(__file__))
     generator_weights_path = os.path.join(abs_dir, arguments.weights_path)
     generated_image = generate_image_from_seed(seed_value,output_path,generator_weights_path)
