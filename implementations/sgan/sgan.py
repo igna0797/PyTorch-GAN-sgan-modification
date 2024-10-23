6e6ad7129c92ce90ff01c7e1d3267e193d083302
igna <igna0797@gmail.com>
2024-10-23 13:31:31 -0300

    missplaced drop_last

--- implementations/sgan/sgan.py
+++ implementations/sgan/sgan.py
@@ -146,11 +146,12 @@ if __name__ == "__main__":
           train=True,
           download=True,
           transform=transforms.Compose(
-              [transforms.Resize(opt.img_size), transforms.ToTensor(),transforms.Normalize([0.5], [0.5] ,drop_last=True])
+              [transforms.Resize(opt.img_size), transforms.ToTensor(),transforms.Normalize([0.5], [0.5])]
           ),
       ),
       batch_size=opt.batch_size,
       shuffle=True,
+      drop_last=True
   )
 
 
