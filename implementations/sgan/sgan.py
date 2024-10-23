58502416ca0aecb2d83d9a9b65d788cc79a143d6
igna <igna0797@gmail.com>
2024-10-23 13:24:26 -0300

    same with the otrher dataloader

--- implementations/sgan/sgan.py
+++ implementations/sgan/sgan.py
@@ -146,7 +146,7 @@ if __name__ == "__main__":
           train=True,
           download=True,
           transform=transforms.Compose(
-              [transforms.Resize(opt.img_size), transforms.ToTensor(),transforms.Normalize([0.5], [0.5] )]
+              [transforms.Resize(opt.img_size), transforms.ToTensor(),transforms.Normalize([0.5], [0.5] ),drop_last=True]
           ),
       ),
       batch_size=opt.batch_size,
